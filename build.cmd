757d4b1d7d5909a0f903e039c7b02281a8a03054
Igal Tabachnik <hmemcpy@gmail.com>
2015-08-13 17:54:20 +0300

    Version bemp.

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ if "%config%" == "" (
    set config=Release
 )
  
-set version=0.1.2
+set version=0.1.3-RC2
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
@@ -19,8 +19,4 @@ if "%nuget%" == "" (
 %nuget% restore src\ReSharper.ReMoji.sln
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" src\ReSharper.ReMoji.sln /t:Rebuild /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Minimal /nr:false
 
-%nuget% pack "src\ReSharper.ReMoji.nuspec" -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=Wave;ReSharperVer=[2.0];SDK=9.1;PackageId=%package_id%" -Verbosity detailed
-REM le sigh
-ren "%package_id%.%version%.nupkg" "%package_id%.%version%-9.1.nupkg"
-%nuget% pack "src\ReSharper.ReMoji.nuspec" -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=Wave;ReSharperVer=[3.0];SDK=9.2;PackageId=%package_id%" -Verbosity detailed
-ren "%package_id%.%version%.nupkg" "%package_id%.%version%-9.2.nupkg"
+%nuget% pack "src\ReSharper.ReMoji.nuspec" -NoPackageAnalysis -Version %version% -Properties "Configuration=%config%;ReSharperDep=Wave;ReSharperVer=[3.0];SDK=9.x;PackageId=%package_id%" -Verbosity detailed
